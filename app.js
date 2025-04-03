25b16ef149c80babd060a3154809937fa2b3f53c
Apple_pine <appchen0517@gmail.com>
2025-04-04 01:19:27 +0800

    upadte: upload and download files + fix eraser

--- app.js
+++ app.js
@@ -6,11 +6,16 @@ let paintColor = "red";
 // not selecting any painters now
 let paintTool = null;
 let isDrawing = false;
+let fontSize = 18;
+let setFontSize = document.getElementById('fontSize');
+let BG = document.getElementById('background');
 let canvas = document.getElementById('canva-area');
 // could draw 2D objects => ctx = context
+let BGctx = BG.getContext('2d')
 let ctx = canvas.getContext('2d');
 let slideBrush = document.getElementById('sizeSlider');
 let slideOpacity = document.getElementById("opacitySlider");
+let inputFile = document.getElementById('fileInput');
 
 const toolboxWidth = document.querySelector('section.tool-box').offsetWidth;
 const paintToolsList = ["pen","eraser","text","circle","rectangle","triangle"];
@@ -56,6 +61,16 @@ slideBrush.addEventListener('change',()=>{
     console.log("brushSize = " + brushSize);
 });
 
+setFontSize.addEventListener("change",()=>{
+    fontSize = setFontSize.value;
+    console.log("change font size to ",fontSize);
+});
+
+// input file call handleImage
+// false -> 按照事情冒泡順序而不是捕獲順序做處理 ! 
+// false is default
+inputFile.addEventListener('change', handleImage, false); 
+
 
 canvas.addEventListener('mousedown', (mouse) => {
     isDrawing = true;
@@ -68,17 +83,30 @@ canvas.addEventListener('mousedown', (mouse) => {
 });
 
 canvas.addEventListener('mousemove', (mouse) => {
-    if (isDrawing) {
+    if (isDrawing) 
+    {
         const rect = canvas.getBoundingClientRect();
         const x = mouse.clientX - rect.left;
         const y = mouse.clientY - rect.top;
 
         ctx.strokeStyle = paintColor;
-        ctx.lineWidth = brushSize;
-        
-        ctx.lineTo(x, y);
-        ctx.moveTo(x, y);
-        ctx.stroke();
+
+        if(paintTool == "pen")
+        {
+            ctx.lineTo(x, y);
+            ctx.moveTo(x, y);
+            ctx.stroke();
+            ctx.lineWidth = brushSize;
+        }
+        else if (paintTool === "eraser") 
+        {
+            ctx.clearRect(x - brushSize/2, y - brushSize/2, brushSize*2, brushSize*2);
+        }
+
+        // else if(paintTool == "text")
+        // {
+            
+        // }
     }
 });
 
@@ -95,6 +123,7 @@ function init()
 {
     canvas.width = canvas.offsetWidth;
     canvas.height = canvas.offsetHeight;
+    ctx.font = "20pt Arial";
     reset();
 }
 
@@ -108,16 +137,41 @@ function onClick(btn)
     }
 
     // upload
-    // else if
-    // {
-
-    // }
+    else if(btn == "upload")
+    {
+        inputFile.click();
+    }
 
     // download
-    // else if
-    // {
+    // ref : https://stackoverflow.com/questions/8126623/downloading-canvas-element-to-an-image
+    else if(btn == "save")
+    {
+        console.log("save img");
+        let combinedCanvas = document.createElement('canvas');
+        combinedCanvas.width = canvas.width;
+        combinedCanvas.height = canvas.height;
+        let combinedCtx = combinedCanvas.getContext('2d');
+    
+        combinedCtx.fillStyle = "white";
+        combinedCtx.fillRect(0,0,canvas.width,canvas.height);
 
-    // }
+        // set background
+        // BGctx.fillStyle = canvaBG;
+        // BG.width = canvas.width;
+        // BG.height = canvas.height;
+        // BGctx.fillRect(0, 0, canvas.width, canvas.height);
+    
+        // combinedCtx.drawImage(BG, 0, 0);
+    
+        // draw the upper one on the combined canva
+        combinedCtx.drawImage(canvas, 0, 0);
+    
+        // a tag in <html>
+        let link = document.createElement('a');
+        link.download = 'masterpiece.png';
+        link.href = combinedCanvas.toDataURL();
+        link.click();
+    }
 
     // undo
     // else if
@@ -139,12 +193,43 @@ function onClick(btn)
 }
 
 
+// ref1: https://stackoverflow.com/questions/10906734/how-to-upload-image-into-html5-canvas
+// ref2: https://jsfiddle.net/influenztial/qy7h5/
+function handleImage(e)
+{
+    /*
+    the order of executing the program
+    reader.readAsDataURL(e.target.files[0]) would trigger reader.onload() 
+    => img.src = event.target.result sent to img to be its src
+    => img.onload() be triggered
+    */
+    let reader = new FileReader();
+    // onload => if the reader and img load successfully than would call this !
+    reader.onload = (event) => {
+        let img = new Image();
+        img.onload = () => {
+            ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+        }
+        // event.target => is the reader. 
+        // so it means that the reader's result (may be base64 or something else)
+        // it would be the source of img.src
+        img.src = event.target.result;
+    }
+
+    // e.target => the one that have this event => <input type="file">! 
+    // files[0] to get the first file
+    reader.readAsDataURL(e.target.files[0]);  
+}
 // reset
 function reset() 
 {
     // fill with the background color
-    ctx.fillStyle = canvaBG;
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-    ctx.strokeStyle = paintColor;
-    ctx.lineWidth = brushSize;
+    BGctx.fillStyle = canvaBG;
+    BG.width = canvas.width;
+    BG.height = canvas.height;
+    BGctx.fillRect(0, 0, canvas.width, canvas.height);
+    // ctx.strokeStyle = paintColor;
+    // ctx.lineWidth = brushSize;
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
 }
